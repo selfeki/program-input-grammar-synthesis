913eafd1da3193f2f2a765202d60fa40095c6eee
selfeki <samielfeki@hotmail.com>
2019-12-06 02:09:55 -0800

    generalize AltNode

--- GrammarSynthesizer.cpp
+++ GrammarSynthesizer.cpp
@@ -1,25 +1,24 @@
-
-
 #include <string>
 #include <list>
 #include <vector>
 #include <functional>
 
+
 class Node {
 public:
-	virtual Grammar accept(NodeVisitor &visitor) = 0;
+	virtual std::vector<Node*> accept(NodeVisitor &visitor) = 0;
 };
 
+
+typedef std::vector<Node*> Grammar;
 
 class RepNode : public Node {
 public:
 	RepNode(std::string s)
 		: terminal{s}
 		{}
-
-	virtual Grammar accept(NodeVisitor &visitor) { return visitor.visit(this); }
-
 	std::string getTerminal() { return terminal; }
+	virtual Grammar accept(NodeVisitor &visitor) { return visitor.visit(this); }
 
 private:
 	Node *parent;
@@ -32,10 +31,8 @@ public:
 	AltNode(std::string s)
 		: terminal{s}
 		{}
-
-	virtual Grammar accept(NodeVisitor& visitor) { return visitor.visit(this); }
-
 	std::string getTerminal() { return terminal; }
+	virtual Grammar accept(NodeVisitor& visitor) { return visitor.visit(this); }
 
 private:
 	Node *parent;
@@ -43,35 +40,17 @@ private:
 };
 
 
-class StarNode : public Node {
+class NonGeneralizableNode : public Node {
 public:
-		StarNode(){}
-
+	NonGeneralizableNode(){}
+	void addChild(Node* node) { grammar.push_back(node); }
+	Grammar getGrammar() { return grammar; }
+	Grammar setGrammar(Grammar newGrammar) { grammar = newGrammar; }
 	virtual Grammar accept(NodeVisitor& visitor) { return visitor.visit(this); }
-
-	std::list<Node*> getChildren() { return children; }
-
-	void addChild(Node* node) { children.push_back(node); }
 
 private:
 	Node* parent;
-	std::list<Node*> children;
-};
-
-
-class PlusNode : public Node {
-public:
-	PlusNode(){}
-
-	virtual Grammar accept(NodeVisitor& visitor) { return visitor.visit(this); }
-
-	std::list<Node*> getChildren() { return children; }
-
-	void addChild(Node* node) { children.push_back(node); }
-	
-private:
-	Node* parent;
-	std::list<Node*> children;
+	Grammar grammar;
 };
 
 
@@ -80,35 +59,37 @@ public:
 	TerminalNode(std::string s)
 		: terminal{s}
 		{}
-
-	virtual Grammar accept(NodeVisitor& visitor) { return visitor.visit(this); }
-
 	std::string getTerminal() { return terminal; }
-	
+	virtual Grammar accept(NodeVisitor& visitor) { return visitor.visit(this); }
+ 
 private:
 	Node* parent;
 	std::string terminal;
 };
+
+
+class StarNode : public NonGeneralizableNode {};
+
+
+class PlusNode : public NonGeneralizableNode {};
+
+
+class NodeVisitor {
+public:
+  virtual Grammar visit(RepNode* repNode) { }
+  virtual Grammar visit(AltNode* altNode) { }
+  virtual Grammar visit(TerminalNode* terminalNode) { }
+	virtual Grammar visit(NonGeneralizableNode* starNode) { }
+};
+
 
 struct Context {
 	std::string left;
 	std::string right;
 };
 
-typedef std::vector<Node*> Grammar;
-
-class NodeVisitor {
-public:
-  virtual Grammar visit(RepNode* repNode) { }
-  virtual Grammar visit(AltNode* altNode) { }
-  virtual Grammar visit(StarNode* starNode) { }
-  virtual Grammar visit(PlusNode* plusNode) { }
-  virtual Grammar visit(TerminalNode* terminalNode) { }
-};
-
 
 class GetContextVisitor: public NodeVisitor {
-
 public:
 	GetContextVisitor(Node* r, Node* t)
 		: root(r),
@@ -164,26 +145,41 @@ private:
 
 class GeneralizeVisitor : public NodeVisitor {
 public:
-	GeneralizeVisitor(Node *r, bool (*predFunc)(std::string))
+	GeneralizeVisitor(Node* r, bool (*predFunc)(std::string))
 	: root(r),
 		oracle(predFunc)
 	{}
-
-	std::vector<std::string>
-	generateResiduals(std::string sub1, std::string sub2, std::string sub3) {
-		return {sub1.append(sub3), sub1.append(sub2).append(sub2).append(sub3)};
-	}
 
 	std::vector<std::string>
 	generateContexts(std::string sub1, std::string sub2, std::string sub3) {
 		return {sub1.append(sub3), sub1.append(sub2).append(sub2).append(sub3)};
 	}
 
-	// std::vector<std::string>
-	// generateChecks() {
-	// 	return
-	// }
+	void
+	replaceGrammarAt(Grammar& grammar, Grammar& newGrammar, Node* toReplace) { // TODO: bug hotspot
+		auto it 		= std::find (grammar.begin(), grammar.end(), toReplace);
+		auto newLoc = grammar.erase(it);
+		grammar.insert(newLoc, newGrammar.begin(), newGrammar.end());
+	}
 
+	Grammar
+	visit(NonGeneralizableNode* nonGenNode) {
+		Grammar grammar = nonGenNode->getGrammar();
+		for (auto it = grammar.rbegin(); it != grammar.rend(); ++it) {
+			Node* node = *it;
+			Grammar resGrammar = node->accept(*this);
+			if (isGeneralized) {
+				replaceGrammarAt(grammar, resGrammar, node);
+				break;
+			}
+		}
+		return {};
+	}
+
+	std::vector<std::string>
+	getRepResiduals(std::string sub1, std::string sub2, std::string sub3) {
+		return {sub1.append(sub3), sub1.append(sub2).append(sub2).append(sub3)};
+	}
 
 	Grammar
 	visit(RepNode* repNode) {
@@ -203,7 +199,7 @@ public:
 				sub1 = alpha.substr(0, i);
 				sub2 = alpha.substr(i, j-i);
 				sub3 = alpha.substr(j, length - j);
-				std::vector<std::string> residuals = generateResiduals(sub1, sub2, sub3);
+				std::vector<std::string> residuals = getRepResiduals(sub1, sub2, sub3);
 
 				for (std::string res : residuals) {
 					std::string check = context.left.append(res).append(context.right);
@@ -220,7 +216,6 @@ public:
 						return candidate;
 					}
 				}
-
 			}
 		}
 		// last resort generalization
@@ -228,9 +223,50 @@ public:
 		isGeneralized = true;
 		return candidate;
 	}
-	virtual void visit(AltNode &altNode) {}
 
-	Node *root;
+
+	Grammar
+	visit(AltNode* altNode) {
+		if (isGeneralized) { return {}; }
+		GetContextVisitor getContextVisitor(root, altNode);
+		root->accept(getContextVisitor);
+		Context context = getContextVisitor.getContext();
+
+		std::string alpha = altNode->getTerminal();
+		std::string sub1, sub2;
+		int length = alpha.length();
+		Grammar candidate;
+		// priority for shorter sub1
+		for (int i = 1; i < length; i++) {
+				sub1 = alpha.substr(0, i);
+				sub2 = alpha.substr(i, length-i);
+				std::vector<std::string> residuals = {sub1, sub2};
+
+				for (std::string res : residuals) {
+					std::string check = context.left.append(res).append(context.right);
+					if (oracle(check)) {
+
+						RepNode* rep	 = new RepNode(sub1);
+						AltNode* alt	 = new AltNode(sub2);
+						PlusNode* plus = new PlusNode();
+						plus->addChild(rep);
+						plus->addChild(alt);
+
+						candidate.push_back(plus);
+						isGeneralized = true;
+						return candidate;
+					}
+				}
+			}
+		}
+		// last resort generalization
+		candidate.push_back(new RepNode(alpha));
+		isGeneralized = true;
+		return candidate;
+	}
+
+private:
+	Node* root;
 	bool isGeneralized = false;
 	std::function<bool (std::string)> oracle;
 	
